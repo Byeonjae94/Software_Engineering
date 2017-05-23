3b1c8725a85fe46b25e966f51abd596cd7b62501
Byeonjae94 <skyrace21@naver.com>
2017-05-23 19:21:31 +0900

    ReceivingTest_Client 파일 푸시한다

--- ReceivingTest_Client.py
+++ ReceivingTest_Client.py
@@ -1,3 +1,20 @@
 import SocketToReceiverTest_Client as Sock
+import time
+import MySQLdb
 
-print(Sock.sendingMsg().split())
+db = MySQLdb.connect('localhost','root','1234567890','TestDB')
+
+cursor = db.cursor()
+sql = ""
+
+while True:
+    time.sleep(5)
+    receiving = Sock.sendingMsg().split("'")
+    for i in range(0,len(receiving)):
+        if i%2==1:
+            li = receiving[i].split(" ")
+            sql = "INSERT INTO TestTable (Pin,Location) VALUES("+li[0]+","+li[1]+");"
+            print(sql)
+            cursor.execute(sql)
+
+db.close()
