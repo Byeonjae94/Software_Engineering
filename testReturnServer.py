5ced14df53317a3b638ad5dc36ce8bfa35acb754
root <root@ip-172-31-1-59.ap-northeast-2.compute.internal>
2017-05-25 14:15:37 +0000

    Serverside Library 추가 및 운동장 테스트 확인

--- testReturnServer.py
+++ testReturnServer.py
@@ -11,31 +11,45 @@ s.listen(1)
 conn, addr=s.accept()
 print('Connected by',addr)
 
-db = MySQLdb.connect('localhost','root','0893','traccardb1')
-cursor = db.cursor()
+#db = MySQLdb.connect('localhost','root','0893','traccardb1')
+#cursor = db.cursor()
 
 while True:
     data=conn.recv(1024)
     if not data:
-         break
-    print('Client requested by entering ',data)
+        break
+
+    db = MySQLdb.connect('localhost','root','0893','traccardb1')
+    cursorPositions = db.cursor()
+    cursorLocations = db.cursor()    
+
+    cursorPositions.execute("select * from (select deviceid,MAX(devicetime) as devicetime from positions group by deviceid) as A join (select deviceid,devicetime,latitude,longitude from positions) as B on A.deviceid = B.deviceid and A.devicetime = B.devicetime;")
     
-    cursor.execute("select * from positions where deviceid=1 order by devicetime DESC limit 1")
-    lastdata = cursor.fetchone()
-    usr_pos = (lastdata[7],lastdata[8])
+    positionRows = cursorPositions.rowcount
+    conn.send(str(positionRows))
 
-    cursor.execute("select * from locations")
-    for i in xrange(cursor.rowcount):
-        place,loc_lati,loc_longi,scope = cursor.fetchone()
-        loc_pos = (loc_lati,loc_longi)
-        distance = vincenty(usr_pos,loc_pos).meters
-	if i==0:
-	    nearest = (place,distance,distance<scope)
-	elif(distance<nearest[1]):
-	    nearest = (place,distance,distance<scope)	
+    for j in xrange(positionRows):
+        lastdata = cursorPositions.fetchone()
+        usr_id = lastdata[2]
+        usr_pos = (lastdata[4],lastdata[5])
 
-    msg = nearest[0]+" "+str(nearest[1])+" "+str(nearest[2])
-    conn.send(msg)
+        cursorLocations.execute("select * from locations")
+    
+        for i in xrange(cursorLocations.rowcount):
+            place,loc_lati,loc_longi,scope,clock = cursorLocations.fetchone()
+            loc_pos = (loc_lati,loc_longi)
+            distance = vincenty(usr_pos,loc_pos).meters
+            if i==0:
+                nearest = (place,distance,distance<scope)
+            elif(distance<nearest[1]):
+                nearest = (place,distance,distance<scope)	
+
+        msg = str(usr_id)+" "+nearest[0]+" "+str(nearest[1])+" "+str(nearest[2])
+        print(msg)
+        conn.send(msg)
+        isReceived = conn.recv(1024)
+        print(isReceived)
+
+    db.close()
 
 conn.close()
-db.close()
